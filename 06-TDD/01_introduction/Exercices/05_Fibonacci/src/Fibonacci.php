dee3a62d2c94de4f2b7607e551bc59a0008ccf74
EricJaslet <ericjaslet@MacBook-Pro-de-Eric.local>
2021-10-06 13:42:59 +0200

    add

--- 06-TDD/01_introduction/Exercices/05_Fibonacci/src/Fibonacci.php
+++ 06-TDD/01_introduction/Exercices/05_Fibonacci/src/Fibonacci.php
@@ -24,4 +24,18 @@ class Fibonacci
             $prev = $temp;
         }
     }
+
+    public function genSimpleSuite($n)
+    {
+        $num1 = 0;
+        $num2 = 1;
+        $counter = 0;
+        echo "\n =>";
+        while ($counter < $n) {
+            echo ' '.$num1;
+            $num3 = $num2 + $num1;
+            [$num1, $num2] = [$num2, $num3];
+            $counter = $counter + 1;
+        }
+    }
 }
