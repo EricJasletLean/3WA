37588c91938429d006514b02b2187fec07d11fbe
EricJaslet <eric.jaslet@lean.fr>
2021-11-02 17:37:32 +0100

    up

---
+++ 08-DESIGN_PATTERN/EXO/01-Cart-observer/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '01e1dcaa066bbc2ff0a8c42b6590434708315cb1',
+        'name' => 'alan/app',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alan/app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '01e1dcaa066bbc2ff0a8c42b6590434708315cb1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ 08-DESIGN_PATTERN/EXO/02-Iterater-generater/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '01e1dcaa066bbc2ff0a8c42b6590434708315cb1',
+        'name' => 'eric/app',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'eric/app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '01e1dcaa066bbc2ff0a8c42b6590434708315cb1',
+            'dev_requirement' => false,
+        ),
+        'php-ds/php-ds' => array(
+            'pretty_version' => 'v1.3.0',
+            'version' => '1.3.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../php-ds/php-ds',
+            'aliases' => array(),
+            'reference' => 'b98396862fb8a13cbdbbaf4d18be28ee5c01ed3c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
