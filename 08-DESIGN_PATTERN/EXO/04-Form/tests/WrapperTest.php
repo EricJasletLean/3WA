68d915647bb837fc65f143cd2cc4429c60164dc6
EricJaslet <eric.jaslet@lean.fr>
2021-11-03 15:03:33 +0100

    up

---
+++ 08-DESIGN_PATTERN/EXO/04-Form/tests/WrapperTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use App\Form\Wrapper;
+use App\Form\Input;
+
+class WrapperTest extends TestCase
+{
+    protected Wrapper $wrapper;
+
+    public function setUp(): void
+    {
+        $this->wrapper = new Wrapper();
+    }
+
+    public function testWrapperIsWrapper()
+    {
+        $this->assertInstanceOf('App\Form\Wrapper', $this->wrapper);
+    }
+
+    // public function testWrapperAddComponent()
+    // {
+    //     $input = new Input('description', "Description", 'text');
+
+    // }
+}
---
+++ 08-DESIGN_PATTERN/EXO/04-Form/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b8f6effad6a9374ee122b636e21b27cf83065d5d',
+        'name' => 'eric/app',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'eric/app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b8f6effad6a9374ee122b636e21b27cf83065d5d',
+            'dev_requirement' => false,
+        ),
+        'php-ds/php-ds' => array(
+            'pretty_version' => 'v1.3.0',
+            'version' => '1.3.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../php-ds/php-ds',
+            'aliases' => array(),
+            'reference' => 'b98396862fb8a13cbdbbaf4d18be28ee5c01ed3c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
